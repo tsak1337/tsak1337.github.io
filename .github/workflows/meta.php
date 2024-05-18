5d55a22c5f86d7b88b93344c470c24ae405c0e61
tsak1337 <yidali0628@outlook.com>
2024-05-18 19:29:23 +0800

    Add files via upload

---
+++ .github/workflows/meta.php
@@ -0,0 +1,18 @@
+<?php
+function metatag($thing1,$thing2, $aditurl){?>
+	<meta name="subject" content="GameSense" />
+    <meta name="description" content="Game4Sense 
+    Made BY fastervac <3" />
+    <meta name="theme-color" content="#6cc312" />
+    <meta name="msapplication-TileColor" content="#6cc312" />
+    <meta name="identifier-URL" content="http://game4sense.rf.gd/<?php echo $aditurl ?>" />
+    <meta name="url" content="http://game4sense.rf.gd/<?php echo $aditurl ?>" />
+    <meta property="og:title" content="GameSense" />
+    <meta property="og:url" content="http://game4sense.rf.gd/<?php echo $aditurl ?>" />
+    <meta property="og:type" content="website" />
+    <meta property="og:image" content="/static/img/icons-512.png" />
+    <meta property="og:image:type" content="image/png" />
+    <meta property="og:description" content="Game4Sense 
+    Made BY fastervac <3" />
+
+<?php }
